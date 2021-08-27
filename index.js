d53d64b6e1f9b9285ff471c9ff2a08bb9b156837
Shubh Agrawal <shubhagrawal1280@gmail.com>
2021-08-27 08:14:38 +0530

    improved ui

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inputElement = document.querySelector("#title-here");
 const finalOutput = document.querySelector("#result");
+const copyButton = document.querySelector(".copy");
+const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
 
 const words = [
   "and",
@@ -57,4 +60,24 @@ function formatText() {
   finalOutput.innerHTML = result.join(" ");
 }
 
+function showAnswer() {
+  answer.classList.toggle("show");
+}
+
+function copyText() {
+  var text = inputElement;
+
+  /* Select the text field */
+  text.select();
+  text.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(text.value);
+
+  /* Alert the copied text */
+  console.log("Copied the text: " + text.value);
+}
+
 inputElement.addEventListener("change", formatText);
+question.addEventListener("click", showAnswer);
+copyButton.addEventListener("click", copyText);
